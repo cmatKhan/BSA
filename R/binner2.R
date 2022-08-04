4ba1d3b3f94898a0fe34dc32e3fff1bfb9c6df91
chase mateusiak <chase.mateusiak@gmail.com>
2022-08-04 15:51:28 -0500

    removing test from binner2

--- R/binner2.R
+++ R/binner2.R
@@ -88,7 +88,3 @@ binner2 <- function(input, bin.size = 10000) {
 }
 
 
-lapply(names(allPoolsZoom),
-       function(x) binner2(allPoolsZoom[[grep(x, names(allPoolsZoom))]], bin.size = chosenSize))
-
-
